ec17cd89467491519047131c5f28a5dc03cc10d9
RafaelPrata <RafaelPrata@users.noreply.github.com>
2020-08-30 17:52:57 -0300

    alterações db

--- db/script dados.js
+++ db/script dados.js
@@ -1,95 +1,95 @@
 ////////////////StatusLeito/////////////////////////////////////
-db.getCollection('StatusPedidoInternacao').insertMany([{'Descricao':'Aberto', 'CodigoStatus':1},
-                                                       {'Descricao':'Em atendimento', 'CodigoStatus':2},
-                                                       {'Descricao':'ConcluÌdo', 'CodigoStatus':3},
+db.getCollection('StatusPedidoInternacao').insertMany([{'Descricao':'Aberto', 'Codigo':1},
+                                                       {'Descricao':'Em atendimento', 'Codigo':2},
+                                                       {'Descricao':'Conclu√≠do', 'Codigo':3},
                                             ]);
                                             
 ////////////////StatusLeito/////////////////////////////////////
-db.getCollection('StatusLeito').insertMany([{'Descricao':'Ocupado', 'CodigoStatus':1},
-                                            {'Descricao':'Vago', 'CodigoStatus':2},
-                                            {'Descricao':'Reservado', 'CodigoStatus':3},
+db.getCollection('StatusLeito').insertMany([{'Descricao':'Ocupado', 'Codigo':1},
+                                            {'Descricao':'Vago', 'Codigo':2},
+                                            {'Descricao':'Reservado', 'Codigo':3},
                                             ]);
                         
 ////////////////TipoLeitos/////////////////////////////////////
-db.getCollection('TipoLeito').insertMany([{'Descricao':'Leito ClÌnico', 'CodigoLeito':1},
-                        {'Descricao':'Leito Cir˙rgico', 'CodigoLeito':2},
-                        {'Descricao':'Leito ObstÈtrico', 'CodigoLeito':3},
-                        {'Descricao':'Leito Pedi·trico', 'CodigoLeito':4},
-                        {'Descricao':'Leito de Isolamento', 'CodigoLeito':5},
-                        {'Descricao':'Leito de Isolamento Reverso', 'CodigoLeito':6},
-                        {'Descricao':'Leito de Cuidados Intensivos (UTI)', 'CodigoLeito':7},
-                        {'Descricao':'Leito de Cuidados Intermedi·rios (UCI)', 'CodigoLeito':8},
-                        {'Descricao':'Leito de RecuperaÁ„o PÛs-AnestÈsica (RPA)', 'CodigoLeito':9},
-                        {'Descricao':'Leito de Apoio para Procedimentos DiagnÛsticos e/ou TerapÍuticos', 'CodigoLeito':10},
-                        {'Descricao':'Leito de UrgÍncia e EmergÍncia', 'CodigoLeito':11},
-                        {'Descricao':'Leito de PrÈ-Parto', 'CodigoLeito':12}
+db.getCollection('TipoLeito').insertMany([{'Descricao':'Leito Cl√≠nico', 'Codigo':1},
+                        {'Descricao':'Leito Cir√∫rgico', 'Codigo':2},
+                        {'Descricao':'Leito Obst√©trico', 'Codigo':3},
+                        {'Descricao':'Leito Pedi√°trico', 'Codigo':4},
+                        {'Descricao':'Leito de Isolamento', 'Codigo':5},
+                        {'Descricao':'Leito de Isolamento Reverso', 'Codigo':6},
+                        {'Descricao':'Leito de Cuidados Intensivos (UTI)', 'Codigo':7},
+                        {'Descricao':'Leito de Cuidados Intermedi√°rios (UCI)', 'Codigo':8},
+                        {'Descricao':'Leito de Recupera√ß√£o P√≥s-Anest√©sica (RPA)', 'Codigo':9},
+                        {'Descricao':'Leito de Apoio para Procedimentos Diagn√≥sticos e/ou Terap√™uticos', 'Codigo':10},
+                        {'Descricao':'Leito de Urg√™ncia e Emerg√™ncia', 'Codigo':11},
+                        {'Descricao':'Leito de Pr√©-Parto', 'Codigo':12}
                        ]);
 ////////////////TipoExames/////////////////////////////////////
-db.getCollection('TipoExame').insertMany([{'Descricao':'Hemograma', 'CodigoExame':1},
-                        {'Descricao':'Colesterol', 'CodigoExame':2},
-                        {'Descricao':'Ureia e Creatinina', 'CodigoExame':3},
-                        {'Descricao':'Papanicolau', 'CodigoExame':4},
-                        {'Descricao':'Exames de urina', 'CodigoExame':5},
-                        {'Descricao':'Exames de fezes', 'CodigoExame':6},
-                        {'Descricao':'Glicemia', 'CodigoExame':7},
-                        {'Descricao':'Transaminases (ALT e AST) ou TGP e TGO', 'CodigoExame':8},
-                        {'Descricao':'TSH e T4 livre', 'CodigoExame':9},
-                        {'Descricao':'TriglicerÌdeos', 'CodigoExame':10},
-                        {'Descricao':'¡cido ˙rico', 'CodigoExame':11},
-                        {'Descricao':'Radiografia convencional (Raio-x)', 'CodigoExame':12},
-                        {'Descricao':'Mamografia', 'CodigoExame':13},
-                        {'Descricao':'Densitometria Ûssea', 'CodigoExame':14},
-                        {'Descricao':'Tomografia computadorizada', 'CodigoExame':15},
-                        {'Descricao':'Resson‚ncia magnÈtica', 'CodigoExame':16},
-                        {'Descricao':'Ultrassonografia', 'CodigoExame':17},
-                        {'Descricao':'Medicina nuclear', 'CodigoExame':18},
-                        {'Descricao':'Angiografia', 'CodigoExame':19}
+db.getCollection('TipoExame').insertMany([{'Descricao':'Hemograma', 'Codigo':1},
+                        {'Descricao':'Colesterol', 'Codigo':2},
+                        {'Descricao':'Ureia e Creatinina', 'Codigo':3},
+                        {'Descricao':'Papanicolau', 'Codigo':4},
+                        {'Descricao':'Exames de urina', 'Codigo':5},
+                        {'Descricao':'Exames de fezes', 'Codigo':6},
+                        {'Descricao':'Glicemia', 'Codigo':7},
+                        {'Descricao':'Transaminases (ALT e AST) ou TGP e TGO', 'Codigo':8},
+                        {'Descricao':'TSH e T4 livre', 'Codigo':9},
+                        {'Descricao':'Triglicer√≠deos', 'Codigo':10},
+                        {'Descricao':'√Åcido ÔøΩrico', 'Codigo':11},
+                        {'Descricao':'Radiografia convencional (Raio-x)', 'Codigo':12},
+                        {'Descricao':'Mamografia', 'Codigo':13},
+                        {'Descricao':'Densitometria √ìssea', 'Codigo':14},
+                        {'Descricao':'Tomografia computadorizada', 'Codigo':15},
+                        {'Descricao':'Resson√¢ncia magn√©tica', 'Codigo':16},
+                        {'Descricao':'Ultrassonografia', 'Codigo':17},
+                        {'Descricao':'Medicina nuclear', 'Codigo':18},
+                        {'Descricao':'Angiografia', 'Codigo':19}
                        ]);
 ////////////////ESPECIALIDADES/////////////////////////////////////
-db.getCollection('Especialidade').insertMany([{'Descricao':'Acupuntura', 'CodigoEspecialidade':1},
-                           {'Descricao':'Alergia e Imunologia', 'CodigoEspecialidade':2},
-                           {'Descricao':'Anestesiologia', 'CodigoEspecialidade':3},
-                           {'Descricao':'Angiologia e Cirurgia Vascular', 'CodigoEspecialidade':4},
-                           {'Descricao':'Cardiologia', 'CodigoEspecialidade':5},
-                           {'Descricao':'Cirurgia Cardiovascular', 'CodigoEspecialidade':6},
-                           {'Descricao':'Cirurgia da Mao', 'CodigoEspecialidade':7},
-                           {'Descricao':'Cirurgia de Cabeca e Pescoco', 'CodigoEspecialidade':8},
-                           {'Descricao':'Cirurgia Geral', 'CodigoEspecialidade':9},
-                           {'Descricao':'Cirurgia Pediatrica', 'CodigoEspecialidade':10},
-                           {'Descricao':'Cirurgia Plastica', 'CodigoEspecialidade':11},
-                           {'Descricao':'Cirurgia Toracica', 'CodigoEspecialidade':12},
-                           {'Descricao':'Clinica Medica', 'CodigoEspecialidade':13},
-                           {'Descricao':'Coloproctologia', 'CodigoEspecialidade':14},
-                           {'Descricao':'Dermatologia', 'CodigoEspecialidade':15},
-                           {'Descricao':'Dor', 'CodigoEspecialidade':16},
-                           {'Descricao':'Endocrinologia e Metabologia', 'CodigoEspecialidade':17},
-                           {'Descricao':'Endoscopia', 'CodigoEspecialidade':18},
-                           {'Descricao':'Gastroenterologia', 'CodigoEspecialidade':19},
-                           {'Descricao':'Genetica Medica', 'CodigoEspecialidade':20},
-                           {'Descricao':'Geriatria', 'CodigoEspecialidade':21},
-                           {'Descricao':'Ginecologia e Obstetricia', 'CodigoEspecialidade':22},
-                           {'Descricao':'Hematologia e Hemoterapia', 'CodigoEspecialidade':23},
-                           {'Descricao':'Homeopatia', 'CodigoEspecialidade':24},
-                           {'Descricao':'Infectologia', 'CodigoEspecialidade':25},
-                           {'Descricao':'Mastologia', 'CodigoEspecialidade':26},
-                           {'Descricao':'Nefrologia', 'CodigoEspecialidade':27},
-                           {'Descricao':'Neurologia', 'CodigoEspecialidade':28},
-                           {'Descricao':'Nutrologia', 'CodigoEspecialidade':29},
-                           {'Descricao':'Oftalmologia', 'CodigoEspecialidade':30},
-                           {'Descricao':'Ortopedia e Traumatologia', 'CodigoEspecialidade':31},
-                           {'Descricao':'Otorrinolaringologia', 'CodigoEspecialidade':32},
-                           {'Descricao':'Patologia', 'CodigoEspecialidade':33},
-                           {'Descricao':'Pediatria', 'CodigoEspecialidade':34},
-                           {'Descricao':'Pneumologia', 'CodigoEspecialidade':35},
-                           {'Descricao':'Reumatologia', 'CodigoEspecialidade':36},
-                           {'Descricao':'Sexologia', 'CodigoEspecialidade':37},
-                           {'Descricao':'Urologia', 'CodigoEspecialidade':38}                           
+db.getCollection('Especialidade').insertMany([{'Descricao':'Acupuntura', 'Codigo':1},
+                           {'Descricao':'Alergia e Imunologia', 'Codigo':2},
+                           {'Descricao':'Anestesiologia', 'Codigo':3},
+                           {'Descricao':'Angiologia e Cirurgia Vascular', 'Codigo':4},
+                           {'Descricao':'Cardiologia', 'Codigo':5},
+                           {'Descricao':'Cirurgia Cardiovascular', 'Codigo':6},
+                           {'Descricao':'Cirurgia da Mao', 'Codigo':7},
+                           {'Descricao':'Cirurgia de Cabeca e Pescoco', 'Codigo':8},
+                           {'Descricao':'Cirurgia Geral', 'Codigo':9},
+                           {'Descricao':'Cirurgia Pediatrica', 'Codigo':10},
+                           {'Descricao':'Cirurgia Plastica', 'Codigo':11},
+                           {'Descricao':'Cirurgia Toracica', 'Codigo':12},
+                           {'Descricao':'Clinica Medica', 'Codigo':13},
+                           {'Descricao':'Coloproctologia', 'Codigo':14},
+                           {'Descricao':'Dermatologia', 'Codigo':15},
+                           {'Descricao':'Dor', 'Codigo':16},
+                           {'Descricao':'Endocrinologia e Metabologia', 'Codigo':17},
+                           {'Descricao':'Endoscopia', 'Codigo':18},
+                           {'Descricao':'Gastroenterologia', 'Codigo':19},
+                           {'Descricao':'Genetica Medica', 'Codigo':20},
+                           {'Descricao':'Geriatria', 'Codigo':21},
+                           {'Descricao':'Ginecologia e Obstetricia', 'Codigo':22},
+                           {'Descricao':'Hematologia e Hemoterapia', 'Codigo':23},
+                           {'Descricao':'Homeopatia', 'Codigo':24},
+                           {'Descricao':'Infectologia', 'Codigo':25},
+                           {'Descricao':'Mastologia', 'Codigo':26},
+                           {'Descricao':'Nefrologia', 'Codigo':27},
+                           {'Descricao':'Neurologia', 'Codigo':28},
+                           {'Descricao':'Nutrologia', 'Codigo':29},
+                           {'Descricao':'Oftalmologia', 'Codigo':30},
+                           {'Descricao':'Ortopedia e Traumatologia', 'Codigo':31},
+                           {'Descricao':'Otorrinolaringologia', 'Codigo':32},
+                           {'Descricao':'Patologia', 'Codigo':33},
+                           {'Descricao':'Pediatria', 'Codigo':34},
+                           {'Descricao':'Pneumologia', 'Codigo':35},
+                           {'Descricao':'Reumatologia', 'Codigo':36},
+                           {'Descricao':'Sexologia', 'Codigo':37},
+                           {'Descricao':'Urologia', 'Codigo':38}                           
                           ]);
                            
 ////////////////PERFIS/////////////////////////////////////
-db.getCollection('Perfil').insertMany([{'Descricao':'Administrador', 'CodigoPerfil' :1},
-{'Descricao':'Atendente', 'CodigoPerfil' :2},
-{'Descricao':'UsuarioSUS', 'CodigoPerfil' :3}]);
+db.getCollection('Perfil').insertMany([{'Descricao':'Administrador', 'Codigo' :1},
+{'Descricao':'Atendente', 'Codigo' :2},
+{'Descricao':'UsuarioSUS', 'Codigo' :3}]);
 
 ////////////////HORARIOS/////////////////////////////////////
 
@@ -106,31 +106,48 @@ while(Number(horaInicial.split(':')[0]) < 20){
                      (intMinutoInicial > 0 ? '00' : '30');
            
     horarios.insert({
-                        'CodigoHorario': i,
+                        'Codigo': i,
                         'HoraInicial': "'" + horaInicial + "'", 
                         'HoraFinal': "'" + horaFinal + "'"
                     });  
    
     i++;
     horaInicial = horaFinal;                 
-}
-
+}
+
+
+
 ////////////////Medicos/////////////////////////////////////
-var medicos = db.getCollection('Medico');
-var especialidades = db.getCollection('Especialidade').find();
-var codigoMedico = 1;
-
-especialidades.forEach(function(esp){
-    
-    for(i=1;i<=3;i++){
-        medicos.insert({'CodigoMedico': codigoMedico,
-                        'Nome': 'MÈdico ' + codigoMedico,
-                        'CRM': 'CRM' + codigoMedico,
-                        'CodigoEspecialidade': esp.CodigoEspecialidade}
-                      );
-                        
-        codigoMedico++;
-    }       
+var medicos = db.getCollection('Medico');
+
+var especialidades = db.getCollection('Especialidade').find();
+
+var codigoMedico = 1;
+
+
+
+especialidades.forEach(function(esp){
+
+    
+
+    for(i=1;i<=3;i++){
+
+        medicos.insert({'Codigo': codigoMedico,
+
+                        'Nome': 'M√©dico ' + codigoMedico,
+
+                        'CRM': 'CRM' + codigoMedico,
+
+                        'CodigoEspecialidade': esp.Codigo}
+
+                      );
+
+                        
+
+        codigoMedico++;
+
+    }       
+
 });
 
 
