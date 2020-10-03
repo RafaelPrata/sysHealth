d2e0d393bcc03b68d6b0606fd0b76dcba5bc54e6
RafaelPrata <RafaelPrata@users.noreply.github.com>
2020-10-03 11:36:30 -0300

    Update DadosTelaCadastroInternacao.ts

--- fontes-sistema/app/ClientApp/src/app/models/responseDTO/DadosTelaCadastroInternacao.ts
+++ fontes-sistema/app/ClientApp/src/app/models/responseDTO/DadosTelaCadastroInternacao.ts
@@ -1,6 +1,39 @@
 import { Dominio } from "../Dominio";
 
-export class DadosTelaCadastroInternacao {  
-    listaClassificao : Dominio[]
-    listaStatus : Dominio[]
+export class DadosTelaCadastroInternacao {
+    listaClassificao: Dominio[]
+    listaStatus: Dominio[]
+    listaEstados: any[]
+
+    constructor() {
+        this.listaEstados = [
+            { "sigla": "AC" },
+            { "sigla": "AL" },
+            { "sigla": "AP" },
+            { "sigla": "AM" },
+            { "sigla": "BA" },
+            { "sigla": "CE" },
+            { "sigla": "DF" },
+            { "sigla": "ES" },
+            { "sigla": "GO" },
+            { "sigla": "MA" },
+            { "sigla": "MT" },
+            { "sigla": "MS" },
+            { "sigla": "MG" },
+            { "sigla": "PA" },
+            { "sigla": "PB" },
+            { "sigla": "PR" },
+            { "sigla": "PE" },
+            { "sigla": "PI" },
+            { "sigla": "RJ" },
+            { "sigla": "RN" },
+            { "sigla": "RS" },
+            { "sigla": "RO" },
+            { "sigla": "RR" },
+            { "sigla": "SC" },
+            { "sigla": "SP" },
+            { "sigla": "SE" },
+            { "sigla": "TO" }
+        ];
+    }
 }
